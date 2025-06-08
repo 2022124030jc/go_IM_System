ee7cd172c5677c0c9a9e5aeb2b8dceb104a7039d
2022124030jc <3062257150@qq.com>
2025-06-08 14:54:12 +0800

    v0.6 修改用户名

--- user.go
+++ user.go
@@ -72,6 +72,25 @@ func (this *User) DoMessage(msg string) {
 		}
 
 		this.server.mapLock.Unlock()
+	} else if len(msg) > 7 && msg[:7] == "rename|" { //更新用户名
+		// 如果消息是"rename|新用户名"，则更新用户名
+		newName := msg[7:]
+		_, ok := this.server.OnlineMap[newName]
+		if ok {
+			// 如果新用户名已存在，发送错误消息
+			this.SendMsg("用户名已存在，请重新输入\n")
+			return
+		} else {
+			// 如果新用户名不存在，更新用户名
+			this.server.mapLock.Lock()
+			delete(this.server.OnlineMap, this.Name) // 删除旧用户名
+			this.Name = newName                      // 更新用户名
+			this.server.OnlineMap[this.Name] = this  // 添加新用户名
+			this.server.mapLock.Unlock()
+
+			// 通知其他用户
+			this.server.Broadcast(this, "已将用户名改为："+this.Name)
+		}
 	} else {
 		// 否则，将消息广播给其他用户
 		this.server.Broadcast(this, msg)
